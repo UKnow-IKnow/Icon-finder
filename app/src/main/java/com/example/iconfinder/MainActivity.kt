c518c0d9ca635fa1685d87f79bfcfc7dd5e15f43
UKnow-IKnow <sarojsahu014@gmail.com>
2022-07-29 00:22:52 +0530

    done with folder structure

--- app/src/main/java/com/example/iconfinder/MainActivity.kt
+++ app/src/main/java/com/example/iconfinder/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.iconfinder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val search = menu?.findItem(R.id.action_search)
+        val searchView = search?.actionView as SearchView
+
+        search.setOnActionExpandListener(object : MenuItem.OnActionExpandListener{
+            override fun onMenuItemActionExpand(p0: MenuItem?): Boolean = true
+
+            override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+
+            }
+        })
     }
 }
